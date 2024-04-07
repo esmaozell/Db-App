8c32820da7530993ae191659d0d5fbcc5b0bdca3
Cenk ÖZEL <cenk.ozel@ogr.dpu.edu.tr>
2024-04-07 12:32:24 +0300

    service added

--- app.js
+++ app.js
@@ -4,6 +4,19 @@ const dbConnection = require("./helper/mysql");
 
 const app = express();
 
+app.get("/students", (req, res) => {
+  dbConnection.query("SELECT * FROM students", (err, results, fields) => {
+    if (err) {
+      console.log("Database query error: ", err);
+    } else {
+      res.status(200).json({
+        status: "success",
+        data: results,
+      });
+    }
+  });
+});
+
 //check db connection
 dbConnection.getConnection((err, connection) => {
   if (err) {
