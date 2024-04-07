041b5fa1718d7bf384b093278f40f29b835ee96d
Cenk ÖZEL <cenk.ozel@ogr.dpu.edu.tr>
2024-04-07 11:45:42 +0300

    db fixed

--- helper/mysql.js
+++ helper/mysql.js
@@ -2,10 +2,10 @@ const mysql = require("mysql2");
 
 const connection = mysql.createPool({
   connectionLimit: 100,
-  host: "localhost",
-  user: "root",
-  password: "password",
-  database: "example-app",
+  host: "lab-db.cdememeakr15.eu-west-1.rds.amazonaws.com",
+  user: "admin",
+  password: "UXMo2rR8Oe4GVU7mh6Dp",
+  database: "lab-db",
 });
 
 module.exports = connection;
